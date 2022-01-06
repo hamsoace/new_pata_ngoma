b2171c82084ee7510f411e7418184f856a832af4
hamsoace <hamsoace@gmail.com>
2022-01-06 19:59:22 +0300

    changes

--- components/PlayerWidget/index.tsx
+++ components/PlayerWidget/index.tsx
@@ -1,18 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Text, Image, View } from 'react-native';
 import { Song } from "../../types";
 import { AntDesign, FontAwesome } from "@expo/vector-icons";
+import { Audio } from "expo-av";
+import { Sound } from "expo-av/build/Audio";
 
 import styles from "./styles";
 
 const song = {
     id: '2',
+    uri: 'https://drive.google.com/uc?export=download&id=1nMC9kUagHCyQ0TDQj4sORIyf2NxOH1vU',
     imageUri: 'https://i.ytimg.com/vi/QwOEfh-SI7g/hqdefault.jpg',
     title: 'Aiseh',
     artist: 'Skillo',
     }
 
 const PlayerWidget = () => {
+    const [sound, setSound] = useState<Sound|null>{null};
+    const [isPlaying, setIsPlaying] = useState<boolean>{true};
+
+    const onPlaybackStatusUpdate = (status) =>{
+        console.log(status);
+    }
+
+    const playCurrentSong = async ()=>{
+        if(sound){
+           await sound.unloadAsync(); 
+        }
+
+        const {sound: newSound} = await Sound.createAsync(
+            {uri: song.uri},
+            {shouldPlay:isPlaying}, 
+            onPlaybackStatusUpdate,
+        )
+
+        setSound(newSound)
+    }
+
+
+    useEffect(() => {
+        playCurrentSong();
+    }, [])
+
+    const onPlayPausePress = async() => {
+        if(!sound){
+            return;
+        }
+
+        if(isPlaying){
+            await sound.stopAsync();
+        }else{
+            await sound.playAsync();
+        }
+    }
+
     return (
         <View style={styles.container}>
             <Image source={{ uri: song.imageUri }} style={styles.image} />
