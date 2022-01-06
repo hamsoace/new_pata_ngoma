f9a20835a2fff9921ce9ca9ccb332bfcaf409d2a
hamsoace <hamsoace@gmail.com>
2022-01-06 14:10:51 +0300

    Album header aligned ok

--- components/AlbumHeader/index.tsx
+++ components/AlbumHeader/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Text, View, Image, TouchableOpacity} from 'react-native';
+import { Text, View, Image, TouchableOpacity } from 'react-native';
 import { Album } from "../../types";
 import styles from "./styles";
 
@@ -10,20 +10,20 @@ export type AlbumHeaderProps = {
 }
 
 const AlbumHeader = (props: AlbumHeaderProps) => {
-    const {album} = props;
-    return(
+    const { album } = props;
+    return (
         <View style={styles.container}>
-            <Image source={{uri: album.imageUri}} style={styles.image}/>
+            <Image source={{ uri: album.imageUri }} style={styles.image} />
             <Text style={styles.name}>{album.name}</Text>
             <View style={styles.creatorContainer}>
                 <Text style={styles.creator}>By {album.by}</Text>
                 <Text style={styles.likes}>{album.numberOfLikes} Likes</Text>
-                <TouchableOpacity>
-                    <View style={styles.button}>
-                        <Text style={styles.buttonText}>PLAY</Text>
-                    </View>
-                </TouchableOpacity>
             </View>
+            <TouchableOpacity>
+                <View style={styles.button}>
+                    <Text style={styles.buttonText}>PLAY</Text>
+                </View>
+            </TouchableOpacity>
         </View>
     )
 }
--- components/AlbumHeader/styles.ts
+++ components/AlbumHeader/styles.ts
@@ -3,6 +3,7 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
   container:{
       alignItems: "center",
+      padding: 20,
   },
   image:{
       width: 200,
