3b19c642a074531e37681ecc8f2a1fc466d24005
hamsoace <hamsoace@gmail.com>
2022-01-06 20:35:25 +0300

    playprogress

--- components/PlayerWidget/index.tsx
+++ components/PlayerWidget/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Text, Image, View } from 'react-native';
+import { Text, Image, View, TouchableOpacity } from 'react-native';
 import { Song } from "../../types";
 import { AntDesign, FontAwesome } from "@expo/vector-icons";
 import { Audio } from "expo-av";
@@ -13,24 +13,29 @@ const song = {
     imageUri: 'https://i.ytimg.com/vi/QwOEfh-SI7g/hqdefault.jpg',
     title: 'Aiseh',
     artist: 'Skillo',
-    }
+}
 
 const PlayerWidget = () => {
-    const [sound, setSound] = useState<Sound|null>{null};
-    const [isPlaying, setIsPlaying] = useState<boolean>{true};
 
-    const onPlaybackStatusUpdate = (status) =>{
-        console.log(status);
+    const [sound, setSound] = useState<Sound|null>(null);
+    const [isPlaying, setIsPlaying] = useState<boolean>(true);
+    const [duration, setDuration] = useState<number|null>(null);
+    const [position, setPosition] = useState<number|null>(null);
+
+    const onPlaybackStatusUpdate = (status) => {
+        setIsPlaying(status.isPlaying);
+        setDuration(status.durationMillis);
+        setPosition(status.positionMillis);
     }
 
-    const playCurrentSong = async ()=>{
-        if(sound){
-           await sound.unloadAsync(); 
+    const playCurrentSong = async () => {
+        if (sound) {
+            await sound.unloadAsync();
         }
 
-        const {sound: newSound} = await Sound.createAsync(
-            {uri: song.uri},
-            {shouldPlay:isPlaying}, 
+        const { sound: newSound } = await Sound.createAsync(
+            { uri: song.uri },
+            { shouldPlay: isPlaying },
             onPlaybackStatusUpdate,
         )
 
@@ -42,30 +47,40 @@ const PlayerWidget = () => {
         playCurrentSong();
     }, [])
 
-    const onPlayPausePress = async() => {
-        if(!sound){
+    const onPlayPausePress = async () => {
+        if (!sound) {
             return;
         }
 
-        if(isPlaying){
+        if (isPlaying) {
             await sound.stopAsync();
-        }else{
+        } else {
             await sound.playAsync();
         }
+    }
+
+    const getProgress = () => {
+        if(sound === null || duration === null || position === null){
+            return 0;
+        }
+
+        return (position/duration) * 100;
     }
 
     return (
         <View style={styles.container}>
             <Image source={{ uri: song.imageUri }} style={styles.image} />
             <View style={styles.rightContainer}>
-            <View style={styles.nameContainer}>
-                <Text style={styles.title}>{song.title}</Text>
-                <Text style={styles.artist}>{song.artist}</Text>
-            </View>
-            <View style={styles.iconContainer}>
-                <AntDesign name="hearto" size={30} color={"white"} />
-                <FontAwesome name="play" size={30} color={"white"} />
-            </View>
+                <View style={styles.nameContainer}>
+                    <Text style={styles.title}>{getProgress()}{song.title}</Text>
+                    <Text style={styles.artist}>{song.artist}</Text>
+                </View>
+                <View style={styles.iconContainer}>
+                    <AntDesign name="hearto" size={30} color={"white"} />
+                    <TouchableOpacity onPress={onPlayPausePress}>
+                        <FontAwesome name={isPlaying ? 'pause' : 'play'} size={30} color={"white"} />
+                    </TouchableOpacity>
+                </View>
             </View>
         </View>
 
